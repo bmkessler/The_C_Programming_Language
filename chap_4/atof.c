cbc3b5bea6bb3b85e25158c6799e2ebae7e99bf2
Brian Kessler <brian.m.kessler@gmail.com>
2013-12-31 17:36:15 -0800

    partially completed atof exponent handling

--- chap_4/atof.c
+++ chap_4/atof.c
@@ -11,14 +11,14 @@ main()
   char line[MAXLINE];
   
   while( getline(line,MAXLINE) > 0)
-    printf("%f\n",atof(line));
+    printf("%g\n",atof(line));
 }
 
 /* atof: convert a string s to double */
 double atof(char s[])
 {
-  double val, power;
-  int i, sign;
+  double val, power,pow;
+  int i, sign,exp;
   for (i = 0; isspace(s[i]); i++)  /* skip white space */
     ;
   sign = (s[i] == '-') ? -1 : 1;
@@ -32,7 +32,14 @@ double atof(char s[])
     val = 10.0 * val + (s[i] - '0');
     power *= 10.0;
   }
-  return sign * val / power;
+  if(s[i] == 'e')
+    i++;
+  for (exp = 0; isdigit(s[i]); i++)
+    exp = 10 * exp + (s[i] - '0');
+  pow = 1.0;
+  for(i=0;i<exp;i++)
+    pow *= 10.0;
+  return sign * val * pow / power;
 }
 
 /* getline: get line into x, return length  */
